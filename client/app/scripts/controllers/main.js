a0ec1c221ea5188010f7c86aa69ff175c8655264
smchaha <ssmmcc1@gmail.com>
2015-05-19 11:30:32 -0600

    adding relative links to the page contents

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,5 +8,9 @@
  * Controller of the dscovrDataApp
  */
 angular.module('dscovrDataApp')
-  .controller('MainCtrl', function ($scope) {
-  });
+	.controller('MainCtrl', function ($scope, $location, $anchorScroll) {
+		$scope.scrollTo = function(id) {
+			$location.hash(id);
+			$anchorScroll();
+		}
+	});
