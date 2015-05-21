c3aa803e294da1ff9e0f542ea83cf368f2bc95c0
smchaha <ssmmcc1@gmail.com>
2015-05-21 12:35:22 -0600

    unit(ish) tests for main and vis controllers

--- client/test/spec/controllers/vis.js
+++ client/test/spec/controllers/vis.js
@@ -8,15 +8,63 @@ describe('Controller: VisCtrl', function () {
   var VisCtrl,
     scope;
 
+	var routeParams = {type: "summary", arg: "3d" };
+
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     VisCtrl = $controller('VisCtrl', {
-      $scope: scope
+      $scope: scope,
+	  $routeParams: routeParams,
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+	it("should have 5 frame sizes in summary_frame_info", function() {
+		expect( Object.keys(scope.summary_frame_info).length ).toBe(5)
+	});
+
+	it("should parse the same url date as it makes", function() {
+		//test a date
+		var testDate = moment();
+		//turn it into a rul
+		var urldate = scope.make_urldate(testDate);
+		//turn it back, compare with original
+		expect( scope.parse_urldate(urldate).toDate().getTime() )
+			.toBe( testDate.toDate().getTime() );
+	});
+
+	it("should have datepicker functions defined", function() {
+		//these guys are needed for the datepicker directive
+		//used in the view
+		expect( scope.dateselect_onchange ).toBeDefined();
+		expect( scope.dateselect_locationchange ).toBeDefined();
+		expect( scope.datepicker_open ).toBeDefined();
+	});
+
+	it("should have get_plotsrc and get_plotsrc_6h functions"), function() {
+		//these functions are in charge of generating the src for
+		//the canned plots
+		expect( scope.get_plotsrc ).toBeDefined();
+		expect( scope.get_plotsrc_6h ).toBeDefined();
+	});
+
+	it("should set selectmode to 0 if routeParams.type is summary", inject(function( $controller ) {
+		var routeParams = { type: "summary" };
+		var ctrl = $controller('VisCtrl', {
+			$scope: scope,
+			$routeParams: routeParams,
+		});
+		expect(scope.selectmode).toBe(0);
+	}));
+
+	it("should set selectmode to 1 if routeParams.type is interactive", inject(function( $controller ) {
+		var routeParams = { type: "interactive" };
+		var ctrl = $controller('VisCtrl', {
+			$scope: scope,
+			$routeParams: routeParams,
+		});
+		expect(scope.selectmode).toBe(1);
+	}));
+
+
 });
