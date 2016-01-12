73386ef30a46f05f0ffe25df8f30ba79cfe00ea3
Stefan Codrescu <ssmmcc1@gmail.com>
2016-01-12 12:54:21 -0700

    adding version 2 of all factory methods, broken most into a commonGet pattern. Also added three retries to each request

--- client/app/scripts/services/dscovrdataaccess.js
+++ client/app/scripts/services/dscovrdataaccess.js
@@ -11,6 +11,27 @@ angular.module('dscovrDataApp')
   .factory('dscovrDataAccess', function ($http, $q) {
     // Service logic
 	var _url_base = "//gis.ngdc.noaa.gov/dscovr-data-access/";
+	var _commonGet = function( url ) {
+		console.log(url);
+		var tries_counter = 0;
+		var deferred = $q.defer();
+		function do_request() {
+			$http.get( url ).success( function( response ) {
+				if (typeof response.data === 'object') {
+					deferred.resolve(response);
+				} else {
+					deferred.reject(response);
+				}
+			}).error( function( response ) {
+				if (tries_counter < 3) {
+					tries_counter++;
+					do_request();
+				} else {
+					deferred.reject("Error " + response.status + " ("+response.error+") : " + response.message);
+			})
+		}
+		return deferred;
+	}
 	
 
     // Public API here
@@ -26,6 +47,10 @@ angular.module('dscovrDataApp')
 				return $q.reject(response.data);
 			});
 		},
+		getProducts2: function() {
+			var url = _url_base + 'products';
+			return _commonGet(url);
+		},
 		getParameters: function() {
 			return $http.get( _url_base + 'parameters').then( function(response) {
 				if (typeof response.data === 'object') {
@@ -36,6 +61,10 @@ angular.module('dscovrDataApp')
 			}, function(response) {
 				return $q.reject(response.data);
 			});
+		},
+		getParameters2: function() {
+			var url = _url_base + 'parameters';
+			return _commonGet(url);
 		},
 		getValues: function(parameters, criteria) {
 			var url = _url_base + 'values?parameters=' + parameters + '&criteria=' + criteria;
@@ -53,16 +82,24 @@ angular.module('dscovrDataApp')
 		getValues2: function(parameters, criteria) {
 			var url = _url_base + 'values?parameters=' + parameters + '&criteria=' + criteria;
 			console.log(url);
+			var tries_counter = 0;
 			var deferred = $q.defer();
-			$http.get( url ).success( function( response ) {
-				if ( response.length < 1 ) {
-					deferred.reject("No data found for request");
-				} else {
-					deferred.resolve(response);
-				}
-			} ).error( function( response ) {
-					deferred.reject("Error " + response.status + " ("+response.error+") : " + response.message);
-			});
+			function do_request() {
+				$http.get( url ).success( function( response ) {
+					if ( response.length < 1 ) {
+						deferred.reject("No data found for request");
+					} else {
+						deferred.resolve(response);
+					}
+				} ).error( function( response ) {
+					if (tries_counter < 3) {
+						tries_counter++;
+						do_request();
+					} else {
+						deferred.reject("Error " + response.status + " ("+response.error+") : " + response.message);
+					}
+				});
+			}
 			return deferred.promise;
 		},
 		getFiles: function(start, end) {
@@ -76,6 +113,10 @@ angular.module('dscovrDataApp')
 			}, function(response) {
 				return $q.reject(response.data);
 			});
+		},
+		getFiles2: function(start, end) {
+			var url = _url_base + 'files?start_date=' + start + '&end_date=' + end;
+			return _commonGet(url);
 		}
 	};
 });
